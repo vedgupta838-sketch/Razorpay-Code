413a05ebdb620eafc5a6a112469962794080af0f
vedgupta838-sketch <vedgupta838@gmail.com>
2025-11-03 01:00:23 +0530

    small changes

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
+  key_id: process.env.RAZORPAY_KEY,
+  key_secret: process.env.RAZORPAY_SECRET,
 });
 
 // ✅ Create order endpoint
